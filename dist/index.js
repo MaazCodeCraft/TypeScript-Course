654f24e516fcc7be686754ecf85b3ecc08b297e1
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-05-25 13:03:20 +0500

    Access Control Keywords

--- dist/index.js
+++ dist/index.js
@@ -2,18 +2,23 @@
 class Account {
     id;
     owner;
-    balance;
+    _balance;
+    nickName;
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0) {
             throw new Error('Invalid Amount');
         }
-        this.balance += amount;
+        this._balance += amount;
+    }
+    getBalance() {
+        return this._balance;
     }
 }
 let account = new Account(1, 'Maaz', 0);
+console.log(account.getBalance());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 class Account {
     readonly id: number;
     owner: string;
-    balance: number;
+    private _balance: number;
     nickName?: string; //optional
 
     constructor (id: number, owner: string, balance: number ) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposit (amount: number) : void {
@@ -15,9 +15,20 @@ class Account {
         if (amount <= 0){
             throw new Error ('Invalid Amount');
         }
-        this.balance += amount;
+        this._balance += amount;
+    }
+
+    /*
+    private calculateTax () {
+        
+    }
+    */
+   
+    getBalance (): number {
+        return this._balance;
     }
 }
 
 let account = new Account (1, 'Maaz', 0);
 // account.id = 0;
+console.log(account.getBalance());
