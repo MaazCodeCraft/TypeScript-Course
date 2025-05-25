4c71755f19ccf28a0f2c107339e435d5b16d8b71
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-05-25 15:31:46 +0500

    Parameter Properties

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 class Account {
-    readonly id: number;
-    owner: string;
-    private _balance: number;
     nickName?: string; //optional
 
-    constructor (id: number, owner: string, balance: number ) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = balance;
+    constructor (
+        public readonly id: number, 
+        public owner: string, 
+        private _balance: number ) {
     }
 
     deposit (amount: number) : void {
