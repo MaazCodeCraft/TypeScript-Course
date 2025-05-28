16860fccb65fbbfd90969f27ba72d0484d767b86
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-05-28 15:32:36 +0500

    Class Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-// Q1
-// Convert the function below to a generic function:
-// function echo(arg) { return arg; } 
-
-function echo<T> (org: T): T {
-    return org;
+function Component (constructor: Function){
+    console.log('Component Decorator Called');
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+        console.log('Inserting in the DOM');
+    }
 }
 
-// Q2
-// When compiling the following piece of code, we get an error saying ‘Property name does not exist on type T’. How can we solve this problem?
-// function printName<T>(obj: T) {   console.log(obj.name); } 
-
-function printName<T extends {name: string}>(obj: T) {   
-    console.log(obj.name); 
-} 
+@Component
+class ProfileComponent {
+}
