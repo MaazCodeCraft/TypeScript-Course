b63d96a713df757f038ff3d8e4025a1155daa1f6
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-05-30 11:42:13 +0500

    Accessor Decorators

--- dist/index.js
+++ dist/index.js
@@ -1,67 +1,68 @@
 "use strict";
-var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
-    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
-    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
-    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
-    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
-    var _, done = false;
-    for (var i = decorators.length - 1; i >= 0; i--) {
-        var context = {};
-        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
-        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
-        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
-        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
-        if (kind === "accessor") {
-            if (result === void 0) continue;
-            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
-            if (_ = accept(result.get)) descriptor.get = _;
-            if (_ = accept(result.set)) descriptor.set = _;
-            if (_ = accept(result.init)) initializers.unshift(_);
-        }
-        else if (_ = accept(result)) {
-            if (kind === "field") initializers.unshift(_);
-            else descriptor[key] = _;
-        }
-    }
-    if (target) Object.defineProperty(target, contextIn.name, descriptor);
-    done = true;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
-    var useValue = arguments.length > 2;
-    for (var i = 0; i < initializers.length; i++) {
-        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
-    }
-    return useValue ? value : void 0;
-};
-function Component(options) {
-    return (constructor) => {
-        console.log('Component Decorator Called');
-        constructor.prototype.option = options;
-        constructor.prototype.uniqueId = Date.now();
-        constructor.prototype.insertInDOM = () => {
-            console.log('Inserting in the DOM');
-        };
+function Capitalize(target, methodName, descriptor) {
+    const orignal = descriptor.get;
+    descriptor.get = function () {
+        const result = orignal?.call(this);
+        return (typeof result === "string") ? result.toUpperCase() : result;
     };
 }
-function Pipe(constructor) {
-    console.log('Pipe Decorators called');
-    constructor.prototype.pipe = true;
-}
-let ProfileComponent = (() => {
-    let _classDecorators = [Component({ selector: '#my-profile' }), Pipe];
-    let _classDescriptor;
-    let _classExtraInitializers = [];
-    let _classThis;
-    var ProfileComponent = class {
-        static { _classThis = this; }
-        static {
-            const _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
-            __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
-            ProfileComponent = _classThis = _classDescriptor.value;
-            if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
-            __runInitializers(_classThis, _classExtraInitializers);
-        }
+function lowerCase(target, methodName, descriptor) {
+    const orignal = descriptor.get;
+    descriptor.get = function () {
+        const result = orignal?.call(this);
+        return (typeof result === "string") ? result.toLowerCase() : result;
     };
-    return ProfileComponent = _classThis;
-})();
+}
+class Person {
+    firstName;
+    lastName;
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+__decorate([
+    lowerCase
+], Person.prototype, "fullName", null);
+class Student extends Person {
+    rollNo;
+    constructor(rollNo, firstName, lastName) {
+        super(firstName, lastName);
+        this.rollNo = rollNo;
+    }
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+__decorate([
+    Capitalize
+], Student.prototype, "fullName", null);
+class Teacher extends Person {
+    id;
+    constructor(id, firstName, lastName) {
+        super(firstName, lastName);
+        this.id = id;
+    }
+    get fullName() {
+        return `Prof. ${this.firstName} ${this.lastName}`;
+    }
+}
+__decorate([
+    Capitalize
+], Teacher.prototype, "fullName", null);
+let person = new Person('Maaz', 'ur Rahman');
+let student = new Student(5, 'Maaz', 'ur Rahman');
+let teacher = new Teacher(5, 'Maaz', 'ur Rahman');
+console.log(person.fullName);
+console.log(student.fullName);
+console.log(teacher.fullName);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,53 @@
-function Log (target: any, methodName: string, descriptor: PropertyDescriptor){
-    const orignal = descriptor.value as Function;
-    descriptor.value = function (...args: string) {
-        console.log('Before');
-        orignal.call(this, ...args);
-        console.log('After');
+function Capitalize (target: any, methodName: string, descriptor: PropertyDescriptor){
+    const orignal = descriptor.get;
+    descriptor.get = function () {
+        const result = orignal?.call(this);
+        return (typeof result === "string")? result.toUpperCase() : result;
     }
-}
+} 
+
+function lowerCase (target: any, methodName: string, descriptor: PropertyDescriptor){
+    const orignal = descriptor.get;
+    descriptor.get = function () {
+        const result = orignal?.call(this);
+        return (typeof result === "string")? result.toLowerCase() : result;
+    }
+} 
 
 class Person {
-    @Log
-    say (message: string) {
-        console.log('Person says ' + message);
+    constructor(public firstName: string, public lastName: string) {}
+
+    @lowerCase
+    get fullName () {
+        return `${this.firstName} ${this.lastName}`;
     }
 }
 
-let peron = new Person ();
-peron.say('Hello');
+class Student extends Person{
+    constructor(public rollNo: number, firstName: string, lastName: string) {
+        super(firstName, lastName);
+    }
+
+    @Capitalize
+    override get fullName () {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Teacher extends Person{
+    constructor(public id: number, firstName: string, lastName: string) {
+        super(firstName, lastName);
+    }
+
+    @Capitalize
+    override get fullName () {
+        return `Prof. ${this.firstName} ${this.lastName}`;
+    }
+}
+
+let person = new Person('Maaz', 'ur Rahman');
+let student = new Student(5, 'Maaz', 'ur Rahman');
+let teacher = new Teacher(5, 'Maaz', 'ur Rahman');
+console.log(person.fullName);
+console.log(student.fullName);
+console.log(teacher.fullName);
