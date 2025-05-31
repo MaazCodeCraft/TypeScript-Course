f910c2dbf2a636eeb56cc6e6bf14b9d81d8bc09a
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-05-31 15:43:34 +0500

    Module Formats

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const shape_1 = require("./shape");
-let circle = new shape_1.Circle(1);
+import { Circle } from "./shape";
+let circle = new Circle(1);
 console.log(circle.radius);
 //# sourceMappingURL=index.js.map
--- dist/shape.js
+++ dist/shape.js
@@ -1,18 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Square = exports.Circle = void 0;
-class Circle {
+export class Circle {
     radius;
     constructor(radius) {
         this.radius = radius;
     }
 }
-exports.Circle = Circle;
-class Square {
+export class Square {
     width;
     constructor(width) {
         this.width = width;
     }
 }
-exports.Square = Square;
 //# sourceMappingURL=shape.js.map
